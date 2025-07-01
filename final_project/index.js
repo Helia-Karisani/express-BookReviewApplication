b1fb76f1f7655affcad88e6ab51a8981142d3180
Helia_Karisani <helia.karisani@gmail.com>
2025-07-01 13:31:32 -0400

    polishing

--- final_project/index.js
+++ final_project/index.js
@@ -4,19 +4,26 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
-const app = express();
+const app = express(); // initialized express app
 
-app.use(express.json());
-
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
-
+app.use(express.json()); // Enables the app to parse incoming JSON payloads in request bodies.
+// Session Middleware for /customer Routes, Session will track users between requests
+app.use("/customer",session({
+    secret:"fingerprint_customer",
+    resave: true, 
+    saveUninitialized: true
+}))
+// Authentication Middleware for /customer/auth/* Routes. Applies authentication check
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
 });
  
 const PORT =5000;
 
+//Mounts customer routes under /customer
+// Mounts general routes at /
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
+//Starts the server and logs a message once it is up.
 app.listen(PORT,()=>console.log("Server is running"));
